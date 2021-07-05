d877d4858ad5371ee335ef76ff9a11b1eeab68c3
si <anhtuanpro6420@gmail.com>
2021-07-05 20:35:25 +0700

    add type package

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World from node');
 });
 const PORT = process.env.PORT || 4000;
 
