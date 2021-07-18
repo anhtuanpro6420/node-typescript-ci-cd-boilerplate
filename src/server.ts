96612f75713bccc97bdb270346631da9c06e4e51
si <anhtuanpro6420@gmail.com>
2021-07-18 17:45:02 +0700

    test with new branch

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World from node (CI/CD) finished, test');
+  res.send('Hello World from node (CI/CD) test branch');
 });
 const PORT = process.env.PORT || 4000;
 
