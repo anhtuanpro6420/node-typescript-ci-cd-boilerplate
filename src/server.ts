5480d84465dd69ff58049006b59bfc62f707bfc8
si <anhtuanpro6420@gmail.com>
2021-07-18 15:47:04 +0700

    update CD

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World from node');
+  res.send('Hello World from node (CI/CD) v2');
 });
 const PORT = process.env.PORT || 4000;
 
